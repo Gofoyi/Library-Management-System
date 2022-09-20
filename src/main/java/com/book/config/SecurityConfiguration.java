093102eb80d2d70e0429b7e171f6c54a07ccf9d0
Gofoyi <zzz810520440@outlook.com>
2022-09-21 00:02:36 +0800

    Back to normal

--- src/main/java/com/book/config/SecurityConfiguration.java
+++ src/main/java/com/book/config/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login11")
+                .logoutSuccessUrl("/login")
                 .and()
                 .csrf().disable()
                 .rememberMe()
